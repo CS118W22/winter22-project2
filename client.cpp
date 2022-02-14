f0d8d18350a37e112569fc0e4d847c26753801a0
Vince Ai <vinceai@outlook.com>
2022-02-14 11:50:12 -0800

    Modify indentation

--- client.cpp
+++ client.cpp
@@ -15,61 +15,61 @@
 
 int main(int argc, char* argv[])
 {
-  if (argc != 4) {
-    std::cerr << "Usage: " << argv[0] << " <HOSTNAME-OR-IP> <PORT> <FILENAME> " << std::endl;
-    return 1;
-  }
+	if (argc != 4) {
+		std::cerr << "Usage: " << argv[0] << " <HOSTNAME-OR-IP> <PORT> <FILENAME> " << std::endl;
+		return 1;
+	}
 
-  std::string server_ip = argv[1];
-  int port = std::stoi(argv[2]);
-  std::string file_path = argv[3];
+	std::string server_ip = argv[1];
+	int port = std::stoi(argv[2]);
+	std::string file_path = argv[3];
 
-  int sock;
+	int sock;
 
-  // create socket fd
-  if ( (sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0 ) {
-    perror("socket");
-    exit(EXIT_FAILURE);
-  }
+	// create socket fd
+	if ( (sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0 ) {
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
 
-  struct sockaddr_in server_addr;
-  memset(&server_addr, 0, sizeof(server_addr));
+	struct sockaddr_in server_addr;
+	memset(&server_addr, 0, sizeof(server_addr));
 
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(port);
-  
-  // Convert IP address from string to binary form
-  if( !inet_pton(AF_INET, server_ip.c_str(), &server_addr.sin_addr)){
-    std::cerr << "Wrong IP address" << std::endl;
-    exit(EXIT_FAILURE);
-  }
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port);
+	
+	// Convert IP address from string to binary form
+	if( !inet_pton(AF_INET, server_ip.c_str(), &server_addr.sin_addr)){
+		std::cerr << "Wrong IP address" << std::endl;
+		exit(EXIT_FAILURE);
+	}
 
-  int ret;
-  if ( (ret = connect(sock, (sockaddr*)&server_addr, sizeof(server_addr))) < 0 ) {
-    perror("connect");
-    exit(EXIT_FAILURE);
-  }
+	int ret;
+	if ( (ret = connect(sock, (sockaddr*)&server_addr, sizeof(server_addr))) < 0 ) {
+		perror("connect");
+		exit(EXIT_FAILURE);
+	}
 
-  std::string hello_msg = "hello";
-  if ( (ret = send(sock, hello_msg.c_str(), hello_msg.length(), 0)) < 0 ) {
-    perror("send");
-    exit(EXIT_FAILURE);
-  }
+	std::string hello_msg = "hello";
+	if ( (ret = send(sock, hello_msg.c_str(), hello_msg.length(), 0)) < 0 ) {
+		perror("send");
+		exit(EXIT_FAILURE);
+	}
 
-  // char* buffer[BUFFER_SIZE];
-  // memset(buffer, 0, sizeof(buffer));
+	// char* buffer[BUFFER_SIZE];
+	// memset(buffer, 0, sizeof(buffer));
 
-  // if ( (ret = recv(sock, buffer, BUFFER_SIZE, 0)) < 0 ) {
-  //   perror("recv");
-  //   exit(EXIT_FAILURE);
-  // }
+	// if ( (ret = recv(sock, buffer, BUFFER_SIZE, 0)) < 0 ) {
+	//   perror("recv");
+	//   exit(EXIT_FAILURE);
+	// }
 
-  // std::cout << "Server: " << buffer << std::endl;
+	// std::cout << "Server: " << buffer << std::endl;
 
-  if ( close(sock) < 0 ) {
-    perror("close");
-    exit(EXIT_FAILURE);
-  }
+	if ( close(sock) < 0 ) {
+		perror("close");
+		exit(EXIT_FAILURE);
+	}
 
-  return 0;
+	return 0;
 }
--- server.cpp
+++ server.cpp
@@ -15,80 +15,80 @@ int sock = -1;
 
 // signal handler
 void sig_handler(int sig) {
-  if (sock > 0) {
-    if ( close(sock) < 0 ) {
-      perror("close");
-      exit(EXIT_FAILURE);
-    }
-  }
-  std::cout << "Good Bye" << std::endl;
-  exit(EXIT_SUCCESS);
+	if (sock > 0) {
+		if ( close(sock) < 0 ) {
+			perror("close");
+			exit(EXIT_FAILURE);
+		}
+  	}
+	std::cout << "Good Bye" << std::endl;
+	exit(EXIT_SUCCESS);
 }
 
 
 int main(int argc, char* argv[])
 {
-  if (argc != 3) {
-    std::cerr << "Usage: " << argv[0] << " <PORT> <FILE-DIR> "  << std::endl;
-    return 1;
-  }
+  	if (argc != 3) {
+    	std::cerr << "Usage: " << argv[0] << " <PORT> <FILE-DIR> "  << std::endl;
+    	return 1;
+  	}
 
-  signal(SIGINT, sig_handler);
-  signal(SIGQUIT, sig_handler);
-  signal(SIGTERM, sig_handler);
+  	signal(SIGINT, sig_handler);
+  	signal(SIGQUIT, sig_handler);
+  	signal(SIGTERM, sig_handler);
 
-  int port = std::stoi(argv[1]);
-  std::string file_path = argv[2];
+  	int port = std::stoi(argv[1]);
+  	std::string file_path = argv[2];
   
-  // reference: https://www.geeksforgeeks.org/udp-server-client-implementation-c/
+  	// reference: https://www.geeksforgeeks.org/udp-server-client-implementation-c/
 
-  // create socket fd
-  if ( (sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0 ) {
-    perror("socket");
-    exit(EXIT_FAILURE);
-  }
+  	// create socket fd
+  	if ( (sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0 ) {
+    	perror("socket");
+    	exit(EXIT_FAILURE);
+  	}
 
-  struct sockaddr_in server_addr, client_addr;
-  memset(&server_addr, 0, sizeof(server_addr));
-  memset(&client_addr, 0, sizeof(client_addr));
+	struct sockaddr_in server_addr, client_addr;
+	memset(&server_addr, 0, sizeof(server_addr));
+	memset(&client_addr, 0, sizeof(client_addr));
 
 
-  //server information
-  server_addr.sin_family = AF_INET; // IPv4
-  server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // IP address
-  server_addr.sin_port = htons(port); // port number
+	//server information
+	server_addr.sin_family = AF_INET; // IPv4
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // IP address
+	server_addr.sin_port = htons(port); // port number
 
-  // bind socket
-  if ( bind(sock, (sockaddr *)&server_addr, sizeof(server_addr)) < 0 ) {
-    perror("bind");
-    exit(EXIT_FAILURE);
-  }
+	// bind socket
+	if ( bind(sock, (sockaddr *)&server_addr, sizeof(server_addr)) < 0 ) {
+		perror("bind");
+		exit(EXIT_FAILURE);
+	}
 
-  socklen_t sock_len;
-  int n;
-  char buffer[BUFFER_SIZE];
-  memset(&buffer, 0, sizeof(buffer));
+	socklen_t sock_len;
+	int n;
+	char buffer[BUFFER_SIZE];
+	memset(&buffer, 0, sizeof(buffer));
 
-  while (1) {
-    sock_len = sizeof(client_addr);
+	while (1) {
+		sock_len = sizeof(client_addr);
 
-    if ( (n = recvfrom(sock, (char *) buffer, BUFFER_SIZE, 0, (sockaddr *)&client_addr, &sock_len)) < 0 )
-    {
-      perror("recvfrom");
-      memset(&buffer, 0, sizeof(buffer));
-      continue;
-    }
+		if ( (n = recvfrom(sock, (char *) buffer, BUFFER_SIZE, 0, (sockaddr *)&client_addr, &sock_len)) < 0 )
+		{
+		perror("recvfrom");
+		memset(&buffer, 0, sizeof(buffer));
+		continue;
+		}
 
-    std::cout << "Client: " << buffer << std::endl;
+		std::cout << "Client: " << buffer << std::endl;
 
-    // std::string hello_msg = "Hello, this is the server";
-    // if ( (sendto(sock, hello_msg.c_str(), hello_msg.length(), 0, (sockaddr *)&client_addr, sock_len)) < 0)
-    // {
-    //   perror("sendto");
-    //   continue;
-    // }
+		// std::string hello_msg = "Hello, this is the server";
+		// if ( (sendto(sock, hello_msg.c_str(), hello_msg.length(), 0, (sockaddr *)&client_addr, sock_len)) < 0)
+		// {
+		//   perror("sendto");
+		//   continue;
+		// }
 
-  }
+	}
 
-  return 0;
+	return 0;
 }
